4affda830c33fa0941522b4e83243debda54ee5a
juliswer <julianswer36@gmail.com>
2022-04-29 21:24:22 -0300

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,33 @@
 import { z } from "zod";
 
-const userSchema = z.object({
+/* const userSchema = z.object({
     email: z.string().email(),
     fullname: z.string(),
     phone: z.number()
 })
 
-const user = {
+const user = { 
     email: "anfgag@agd.com",
     fullname: "anfgag",
     phone: 123
 }
 
 const res = userSchema.parse(user);
+console.log(res); */
+
+const UserSchema = z.object({
+  email: z.string().email(),
+  fullname: z.string(),
+  phone: z.number(),
+});
+
+type UserType = z.infer<typeof UserSchema>
+
+const UserInput: UserType = {
+    email: "adgadg@gad.com",
+    fullname: "adgadg",
+    phone: 123
+}
+
+const res = UserSchema.parse(UserInput);
 console.log(res);
